6c3c32ce2e5eb7289963d7892b09596fc633800c
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2017-11-19 16:55:58 +0000

    Clean up

--- gradient_check.py
+++ gradient_check.py
@@ -7,7 +7,13 @@ from chainer import gradient_check
 from chainer import Variable
 
 
-def _xp(device):
+def _as_iterable(x):
+    if hasattr(x, '__iter__'):
+        return x
+    return x,
+
+
+def _get_array_module(device):
     if device < 0:
         return numpy
     elif cuda.available:
@@ -16,28 +22,15 @@ def _xp(device):
     raise ValueError('Tried to use the GPU but CuPy was not available.')
 
 
-def _as_iterable(x):
-    if hasattr(x, '__iter__'):
-        return x
-    return x,
-
-
-def _get_ndarray_uniform_sampler(device, dtype='f'):
-    xp = _xp(device)
-    return lambda shape: xp.random.uniform(-1, 1, shape, dtype=dtype)
-
-
 def _sample_ndarrays(sampler, shapes):
     if callable(sampler):
-        arr = tuple(sampler(shape) for shape in shapes)
+        return tuple(sampler(shape) for shape in shapes)
     elif len(sampler) == len(shapes):
-        arr = tuple(
+        return tuple(
             _sampler(shape) for _sampler, shape in zip(sampler, shapes))
-    else:
-        raise ValueError(
-            'Array specific generators were given but the number of'
-            'generators did not match the number of inputs.')
-    return arr
+    raise ValueError(
+        'Array specific generators were given but the number of'
+        'generators did not match the number of inputs.')
 
 
 def _as_function(f, df):
@@ -73,7 +66,7 @@ def _as_function_node(f, df):
 
 def check_backward(f, df, input_shapes, make_inputs=None,
                    make_grad_outputs=None, inputs=None, grad_outputs=None,
-                   device=-1, atol=1e-5, rtol=1e-4, eps=1e-3, no_grads=None):
+                   device=-1, eps=1e-3, atol=1e-5, rtol=1e-4, no_grads=None):
     """Check the gradient computations of the given function.
 
     Inputs and upstream gradients are randomly generated to test for the
@@ -93,31 +86,32 @@ def check_backward(f, df, input_shapes, make_inputs=None,
     """
     def _make_ndarrays(_sampler, _shapes):
         if _sampler is None:
-            _sampler = _get_ndarray_uniform_sampler(device, dtype='f')
+            xp = _get_array_module(device)  # NumPy or CuPy
+            _sampler = lambda shape: xp.random.uniform(-1, 1, shape, dtype='f')
         return _sample_ndarrays(_sampler, _shapes)
 
     if inputs is None:
         inputs = _make_ndarrays(make_inputs, input_shapes)
 
-    outputs = f(*inputs)
-    output_shapes = [output.shape for output in outputs]
     if grad_outputs is None:
+        output_shapes = [output.shape for output in f(*inputs)]
         grad_outputs = _make_ndarrays(make_grad_outputs, output_shapes)
 
     # Use `chainer.function.Function` or `chainer.function_node.FunctionNode`
     # depnding on the class types of the outputs of `df`.
     grads = df(*inputs, *grad_outputs)
-    grads = filter(lambda g: g is not None, grads)
-    is_grad_variables = [isinstance(g, Variable) for g in grads]
-    is_grad_ndarrays = [
+    grads = [g for g in grads if g is not None]
+
+    is_grads_variable = [isinstance(g, Variable) for g in grads]
+    is_grads_ndarray = [
         isinstance(g, (numpy.ndarray, cuda.ndarray)) for g in grads]
 
-    if all(is_grad_variables):
+    if all(is_grads_variable):
         func = _as_function_node(f, df)
-    elif all(is_grad_ndarrays):
+    elif all(is_grads_ndarray):
         func = _as_function(f, df)
     else:
-        raise ValueError('Cannot mix Variables and NumPy/CuPy ndarrays.')
+        raise ValueError('Cannot mix Variable and NumPy/CuPy gradients.')
 
     gradient_check.check_backward(
         func, inputs, grad_outputs, eps=eps, atol=atol, rtol=rtol,
